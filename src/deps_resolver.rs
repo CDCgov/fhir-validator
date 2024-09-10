4354312bce4de98a521a7937d7ff2376f48ed9f4
Cosmin P <lrp4@cdc.gov>
2024-09-10 16:02:50 -0400

    adding child deps to result

--- src/deps_resolver.rs
+++ src/deps_resolver.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 use serde_json::Value;
+// TODO: used to check a dep path is available, use std::path::Path;
 
 type Dependencies = HashSet<String>;
 
@@ -10,8 +11,9 @@ type Dependencies = HashSet<String>;
 pub fn obtain_package_deps(package_folder: &str, package_coordinate: &str) -> Result<Dependencies, Box<dyn Error>> {
 
     let root_deps = get_package_deps(package_folder, package_coordinate)?;
+    let full_deps_tree = dependency_resolver(package_folder, HashSet::new(), root_deps)?;
 
-    Ok(root_deps)
+    Ok(full_deps_tree)
 }// .obtain_package_deps 
 
 
@@ -28,6 +30,12 @@ fn get_package_deps(package_folder: &str, package_coordinate: &str) -> Result<De
 fn get_package_meta(packages_folder: &str, package_coordinate: &str) -> Result<Value, Box<dyn Error>> {
     
     let file_path = format!("./{}/{}_package.ndjson", packages_folder, package_coordinate); 
+    
+    // TODO: skip if not exist or block?
+    //let path_exists = Path::new(&file_path).exists();
+    
+    // TODO: when a needed package is not available in the ig-packages folder 
+    //println!("path_exists: {path_exists} for file_path: {file_path}");
 
     get_specific_line_from_ndjson(&file_path, 1) 
 }// .get_package_meta
@@ -66,4 +74,40 @@ fn normalize_package_deps(package_deps: Option<&Vec<Value>>) -> Dependencies {
     }// .else 
 
 }// .normalize_package_deps
+ 
+
+fn dependency_resolver(package_folder: &str, mut visited_deps: Dependencies, mut enqueued_deps: Dependencies) 
+    -> Result<Dependencies, Box<dyn Error>> {
+        //println!("visited_deps: {:?}", visited_deps);
+        //println!("enqueued_deps: {:?}", enqueued_deps);
+
+        while !enqueued_deps.is_empty() {
+            let mut new_deps = HashSet::new();
+            
+            for dep in enqueued_deps.drain() {
+                let child_deps = get_package_deps(package_folder, &dep)?;
+
+                visited_deps.insert(dep);
+
+                //println!("child_deps: {:?}", child_deps);
+
+                for child_dep in child_deps {
+                    if !visited_deps.contains(&child_dep) {
+                        new_deps.insert(child_dep);
+                    }
+                }// .for
+            }// .for
+
+            //println!("visited_deps: {:?}", visited_deps);
+
+            visited_deps.extend(new_deps.clone());
+
+            //println!("visited_deps(after clone): {:?}", visited_deps);
+
+            enqueued_deps = new_deps;
+        }// .while
+
+        Ok(visited_deps)
+}// .dependency_resolver
+
 
--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Started FHIR Validator based on FHIR Schema...");
     
     let package_folder = "ig-packages";
-    let package_coordinate = "hl7.fhir.us.core#3.1.1";
-    //let package_coordinate = "hl7.fhir.us.health-care-surveys-reporting#1.0.0";
+    // let package_coordinate = "hl7.fhir.us.core#3.1.1";
+    let package_coordinate = "hl7.fhir.us.health-care-surveys-reporting#1.0.0";
 
     println!("Set up validator for profile: {}", package_coordinate);
 
-    let deps = deps_resolver::obtain_package_deps(package_folder, package_coordinate)?;
+    let deps= deps_resolver::obtain_package_deps(package_folder, package_coordinate)?;
     println!("Result dependencies: {:?}", deps);
  
     Ok(())
